a22cfbb2603bc9a810b826a3211dfab9715c1771
pavelchupin9416 <pavelchupin94@yandex.ru>
2019-02-07 01:49:08 +0300

    1.3. JavaDoc. Code convention[#109360]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+*Class Calculate решение задачи части 001 урок 1.3
+*@author chupin
+*@since 07.02.2019
+*/
+
 public class Calculate {
 	
+	/**
+	*Конструктор, вывод строки в консоль.
+	*@param args - args
+	*/
 	public static void main (String[] args){
 		System.out.println("Hello world");
 	}
