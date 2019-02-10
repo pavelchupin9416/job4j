9e9c7a0b09ac4ed98fb7362fbe7b70e5a7a58819
pavelchupin9416 <pavelchupin94@yandex.ru>
2019-02-10 12:13:54 +0300

    1.4. JUnit[#109361]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -6,13 +6,23 @@ package ru.job4j.calculate;
 *@since 07.02.2019
 */
 
+
 public class Calculate {
-	
-	/**
-	*Конструктор, вывод строки в консоль.
-	*@param args - args
-	*/
-	public static void main (String[] args){
-		System.out.println("Hello world");
-	}
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
+ 
