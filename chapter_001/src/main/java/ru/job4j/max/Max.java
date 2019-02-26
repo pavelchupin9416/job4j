4fed2017d708ae5c88dc73780c8e601c15e7c246
pavelchupin9416 <pavelchupin94@yandex.ru>
2019-02-26 08:33:43 +0300

    4.2. Максимум из двух чисел[#109324]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,21 @@
+package ru.job4j.max;
+
+/**
+ * @author Pavel Chupin (pavelchupin94@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Max {
+
+    /**
+     * max - выбор максимального числа.
+     * @param first - первый аргумент.
+     * @param second - второй аргумент.
+     */
+
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+
+}
