94ad4f6d0a0f8e3a40ca80d761c46e6744a2cea9
pavelchupin9416 <pavelchupin94@yandex.ru>
2019-04-28 11:10:19 +0300

    Каркас шахматной доски[#109430]

--- chess/src/main/java/ru/job4j/chess/Chess.java
+++ chess/src/main/java/ru/job4j/chess/Chess.java
@@ -65,13 +65,10 @@ public class Chess extends Application {
         rect.setOnMouseReleased(
                 event -> {
                     try {
-                        if (logic.move(this.findBy(momento.getX(), momento.getY()), this.findBy(event.getX(), event.getY()))) {
+                      logic.move(this.findBy(momento.getX(), momento.getY()), this.findBy(event.getX(), event.getY()));
                             rect.setX(((int) event.getX() / 40) * 40 + 5);
                             rect.setY(((int) event.getY() / 40) * 40 + 5);
-                        } else {
-                            rect.setX(((int) momento.getX() / 40) * 40 + 5);
-                            rect.setY(((int) momento.getY() / 40) * 40 + 5);
-                        }
+
                     } catch (ImpossibleMoveException | OccupiedWayException e) {
                         System.out.println("Не допустимый ход");
                         rect.setX(((int) momento.getX() / 40) * 40 + 5);
