8088b88df4258368cc52aabe627d62da76dbb08b
pavelchupin9416 <pavelchupin94@yandex.ru>
2019-05-16 07:42:50 +0300

    1. Телефонный справочник на базе ArrayList[#109383]

---
+++ chapter_003/src/main/java/ru/job4j/phonedictionary/PhoneDictionary.java
@@ -0,0 +1,35 @@
+package ru.job4j.phonedictionary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *Class PhoneDictionary решение задачи Телефонный справочник на базе ArrayList[#109383].
+ *@author chupin
+ *@since 16.05.2019
+ */
+
+
+public class PhoneDictionary {
+    private List<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+    public List<Person> find(String key) {
+        List<Person> result = new ArrayList<>();
+        for (Person value : persons) {
+           if (value.getName().contains(key) || value.getAddress().contains(key) || value.getPhone().contains(key)
+           || value.getSurname().contains(key)) {
+            result.add(value);
+           }
+        }
+        return result;
+    }
+}
