1950fda722c629e75a59135664df8b2302f56e08
pavelchupin9416 <pavelchupin94@yandex.ru>
2019-06-25 07:31:34 +0300

    2. Сортировка User с использованием Comparator[#109376]

--- chapter_003/src/main/java/ru/job4j/sort/UserComparator.java
+++ chapter_003/src/main/java/ru/job4j/sort/UserComparator.java
@@ -5,16 +5,7 @@ import java.util.Comparator;
 public class UserComparator implements Comparator<User> {
 
     public int compare(User a, User b) {
-        int rst;
-        if (a.getName().length() > b.getName().length()) {
-            rst = 1;
-        } else {
-            if (a.getName().length() == b.getName().length()) {
-                rst = 0;
-            } else {
-                rst = -1;
-            }
-        }
+        int rst = Integer.compare(a.getName().length(), b.getName().length());
         return rst;
     }
 }
