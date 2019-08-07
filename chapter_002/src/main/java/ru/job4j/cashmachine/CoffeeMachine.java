9ce37e2109d5aeeb5584803b4a14b02e56c6c618
pavelchupin9416 <pavelchupin94@yandex.ru>
2019-08-08 00:26:28 +0300

    Кофемашина

--- chapter_002/src/main/java/ru/job4j/cashmachine/CoffeeMachine.java
+++ chapter_002/src/main/java/ru/job4j/cashmachine/CoffeeMachine.java
@@ -7,33 +7,17 @@ public class CoffeeMachine {
 
     private final int[] values = {10, 5, 2, 1};
 
-
     public List<Integer> change(int value, int price) {
-        int note = value - price;
-       List<Integer> result = new ArrayList<>();
-        int r = 0;
-        for (int i = 0; i < this.values.length; i++) {
-            if (note >= this.values[i]) {
-                r = i;
-                break;
+        List<Integer> result = new ArrayList<>();
+        for (int i : values) {
+            while ((value - price) / i > 0) {
+                int step = (value - price) / i;
+                for (int j = 0; j < step; j++) {
+                   result.add(i);
+                }
+                value = value - i * step;
             }
         }
-            int index = r;
-            int temp = note;
-            int steps = temp / this.values[index];
-            List<Integer> list = new ArrayList<>();
-            while (index < this.values.length) {
-                if (temp >= this.values[index]) {
-                    int step = temp / this.values[index];
-                    for (int i = 0; i < step; i++) {
-                        list.add(this.values[index]);
-                    }
-                    temp = temp % this.values[index];
-                } else {
-                    index++;
-                }
-            }
-            result = list;
         return result;
     }
 }
