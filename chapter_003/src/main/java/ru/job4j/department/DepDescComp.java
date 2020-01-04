a46f91574844d290bb4d460cc659e9610b057deb
pavelchupin9416 <pavelchupin94@yandex.ru>
2020-01-04 13:11:09 +0300

    Отсортировать департаменты.

--- chapter_003/src/main/java/ru/job4j/department/DepDescComp.java
+++ chapter_003/src/main/java/ru/job4j/department/DepDescComp.java
@@ -5,9 +5,8 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        String o1t = o1.split("/")[0];
-        String o2t = o2.split("/")[0];
-        int rst = o2t.compareTo(o1t);
-        return rst;
+        String o1t = !o1.contains("/") ? o1 : o1.split("/")[0];
+        String o2t = !o2.contains("/") ? o2 : o2.split("/")[0];
+        return o2t.compareTo(o1t);
     }
 }
