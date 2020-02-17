ae554fe281f2e858b84239f5b58d4d907f1fafd5
pavelchupin9416 <pavelchupin94@yandex.ru>
2020-02-17 22:11:17 +0300

    Отсортировать департаменты.

--- chapter_003/src/main/java/ru/job4j/department/DepDescComp.java
+++ chapter_003/src/main/java/ru/job4j/department/DepDescComp.java
@@ -5,11 +5,28 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        String o1t = !o1.contains("/") ? o1 : o1.split("/")[0];
-        String o2t = !o2.contains("/") ? o2 : o2.split("/")[0];
-        int rst = o2t.compareTo(o1t);
+        int rst = 0;
+        int temp = 0;
+        int first = o1.length() > o2.length() ? o2.length() : o1.length();
+        for (int i = 0; i < first; i++) {
+            if (temp == 0) {
+                if (Character.compare(o2.charAt(i), o1.charAt(i)) != 0) {
+                    rst = Character.compare(o2.charAt(i), o1.charAt(i));
+                    break;
+                }
+            }
+            if (temp == 1) {
+                if (Character.compare(o1.charAt(i), o2.charAt(i)) != 0) {
+                    rst = Character.compare(o1.charAt(i), o2.charAt(i));
+                    break;
+                }
+            }
+            if (String.valueOf(o1.charAt(i)).equals("/")) {
+                temp = 1;
+            }
+        }
         if (rst == 0) {
-            rst = o1.compareTo(o2);
+            rst = Integer.compare(o1.length(), o2.length());
         }
         return rst;
     }
