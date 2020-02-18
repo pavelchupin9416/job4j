ac88ccc77a9cac2bcccec1ea8a2d64eea1300435
pavelchupin9416 <pavelchupin94@yandex.ru>
2020-02-18 21:34:17 +0300

    Отсортировать департаменты.

--- chapter_003/src/main/java/ru/job4j/department/DepDescComp.java
+++ chapter_003/src/main/java/ru/job4j/department/DepDescComp.java
@@ -6,23 +6,19 @@ public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
         int rst = 0;
-        int temp = 0;
+
         int first = o1.length() > o2.length() ? o2.length() : o1.length();
-        for (int i = 0; i < first; i++) {
-            if (temp == 0) {
+        int temp = o1.indexOf("/") != -1 ? o1.indexOf("/") : first;
+        for (int i = 0; i < temp; i++) {
                 if (Character.compare(o2.charAt(i), o1.charAt(i)) != 0) {
                     rst = Character.compare(o2.charAt(i), o1.charAt(i));
                     break;
                 }
-            }
-            if (temp == 1) {
-                if (Character.compare(o1.charAt(i), o2.charAt(i)) != 0) {
-                    rst = Character.compare(o1.charAt(i), o2.charAt(i));
-                    break;
-                }
-            }
-            if (String.valueOf(o1.charAt(i)).equals("/")) {
-                temp = 1;
+        }
+        for (int i = temp; i < first; i++) {
+            if (Character.compare(o1.charAt(i), o2.charAt(i)) != 0) {
+                rst = Character.compare(o1.charAt(i), o2.charAt(i));
+                break;
             }
         }
         if (rst == 0) {
