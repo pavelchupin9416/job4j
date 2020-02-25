05c2a8a1e5a85f2ed71a741af3d927f95f3e976f
pavelchupin9416 <pavelchupin94@yandex.ru>
2020-02-25 22:12:26 +0300

    1. Collection API Улучшения

--- chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
@@ -14,7 +14,7 @@ public class ConvertList2ArrayTest {
     public void when7ElementsThen9() {
         ConvertList2Array list = new ConvertList2Array();
         int[][] result = list.toArray(
-                Arrays.asList(1, 2, 3, 4, 5, 6, 7),
+                List.of(1, 2, 3, 4, 5, 6, 7),
                 3
         );
         int[][] expect = {
@@ -28,12 +28,12 @@ public class ConvertList2ArrayTest {
     @Test
     public void convertlist() {
         ConvertList2Array convertList = new ConvertList2Array();
-        List<int[]> list = new ArrayList<>();
-        list.add(new int[]{1, 2});
-        list.add(new int[]{3, 4, 5, 6});
+        List<int[]> list = List.of(
+        new int[]{1, 2},
+        new int[]{3, 4, 5, 6});
         List<Integer> result = convertList.convert(list);
 
-        List<Integer> expect =  Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> expect =  List.of(1, 2, 3, 4, 5, 6);
                 // будет содержать элементы: (
 
 
--- chapter_003/src/test/java/ru/job4j/list/UserConvertTest.java
+++ chapter_003/src/test/java/ru/job4j/list/UserConvertTest.java
@@ -5,19 +5,20 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class UserConvertTest {
     @Test
     public void mapConvert() {
         UserConvert userConvert = new UserConvert();
-        List<User> list = Arrays.asList(
+        List<User> list = List.of(
                 new User(1, "Вася", "Москва"),
                 new User(2, "Николай", "Питер")
         );
-        HashMap<Integer, User> expect = new HashMap<>();
-         expect.put(1, (new User(1, "Вася", "Москва")));
-         expect.put(2, (new User(2, "Николай", "Питер")));
+        Map<Integer, User> expect = Map.of(
+        1, new User(1, "Вася", "Москва"),
+        2, new User(2, "Николай", "Питер"));
         HashMap<Integer, User> result = userConvert.process(list);
         result.equals(expect);
     }
